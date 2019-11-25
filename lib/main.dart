f2afe59d6c087adb984e4930775206d703945e5d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-11-25 15:09:11 +0100

    add home and body

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home : Text('hey nibbas'),
+  home : Scaffold(
+    appBar: AppBar(
+      title: Text('My Application'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('My Body App'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
 
