ef7bc250f688e20715ddeb9d1c51cd33c53a8566
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-11-26 12:51:58 +0100

    update class Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home : Scaffold(
-    appBar: AppBar(
-      title: Text('My Application'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
+  home : Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Application'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
           'My Body App',
           style: TextStyle(
             fontSize: 20.0,
@@ -17,19 +23,13 @@ void main() => runApp(MaterialApp(
             color: Colors.grey[600],
             fontFamily: 'IndieFlower',
           ),
+        ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-));
-
-class test extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('clickez'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
