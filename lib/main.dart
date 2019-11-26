37d9c3963e6b3ea276fe5ff39739d1484adecb3f
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-11-26 14:25:01 +0100

    add image from assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Image(
-          image: NetworkImage('https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=335&q=80'),
+          image: AssetImage('assets/de-jesus-benitez-kEgJVDkQkbU-unsplash.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
